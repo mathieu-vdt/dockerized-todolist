6a96ab959145386974dd9f3b5b14ffc7434ddbc8
TeyKz <81241850+TeyKz@users.noreply.github.com>
2024-05-31 17:51:51 +0200

    🚨 Wait db when start

--- backend/scripts-shell/wait.sh
+++ backend/scripts-shell/wait.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Waiting for DB
+while ! nc -z db 3306; do
+  echo "local -> Waiting for the MySQL Server"
+  sleep 3
+done
+
+python /app/manage.py migrate
 python /app/manage.py makemigrations
 python /app/manage.py migrate
 
